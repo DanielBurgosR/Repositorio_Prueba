9e527ea89ae437e630f8bc724868f2e55da02110
DanielBurgosR <danielburgos654@gmail.com>
2025-12-17 00:00:41 -0500

    cambio a los 3 archivos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('h1').style.color = '#333333';
+document.querySelector('h1').style.color = '#9c7878ff';
 document.querySelector('h1').style.fontSize = '36px';
 document.querySelector('h1').style.textAlign = 'center';
 document.querySelector('h1').style.marginBottom = '20px';
