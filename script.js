896386ed19fe6587b8986a9c6884cc336b9c3ee4
DanielBurgosR <danielburgos654@gmail.com>
2025-12-16 23:57:29 -0500

    inicio prueba

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('h1').style.color = '#333333';
+document.querySelector('h1').style.fontSize = '36px';
+document.querySelector('h1').style.textAlign = 'center';
+document.querySelector('h1').style.marginBottom = '20px';
